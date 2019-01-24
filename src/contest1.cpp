bb3907bfe480a2279bf612bd97edb334ad6846f4
yuchen-lu <yuchen.lu@mail.utoronto.ca>
2019-01-24 12:45:36 -0600

    1st mapping

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -1,5 +1,5 @@
 #include <ros/console.h>
-#include "ros/ros.h"
+#include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <sensor_msgs/LaserScan.h>
@@ -22,7 +22,7 @@ using namespace std;
 // global variables declarations
 double angular = 0.0, linear = 0.0;
 //odom variables
-double posX, posY, yaw;
+double posX=0, posY=0, yaw=0;
 double pi=3.1416;
 //bumper variables
 bool bumperLeft = 0, bumperCenter = 0, bumperRight = 0;
@@ -30,6 +30,10 @@ bool bumperLeft = 0, bumperCenter = 0, bumperRight = 0;
 //LaserScan variables
 double laserRange = 10;
 int laserSize = 0, laserOffset = 0, desiredAngle = 5;
+int laserIndex;
+
+bool leftTurn, rightTurn;
+
 
 
 // callback functions defined below
@@ -42,7 +46,7 @@ if (msg.bumper == 0)
 	bumperLeft = !bumperLeft;
 else if (msg.bumper == 1)
 	bumperCenter = !bumperCenter;
-else if (msg.bumper == 1)
+else if (msg.bumper == 2)
 	bumperRight = !bumperRight;
 	
 }
@@ -63,26 +67,37 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	// done by finding the smallest distance in the desired field of view
 	// The desired field of view is defined by a negative and positive laseroffset from the heading angle of the robot.
 	//  If the desired field of view > sensor's, use sensor's instead
-	
+
 	laserRange = 11;
 	if (desiredAngle*pi/180 <msg->angle_max && -desiredAngle*pi/180 >msg->angle_min){
 		for (int i =laserSize/2 - laserOffset; i<laserSize/2 + laserOffset; i++){
 			if (laserRange > msg->ranges[i])
 				laserRange = msg->ranges[i];
+				laserIndex = i;
 		}
+		
+		
 	}
 	else
 	{
 		for ( int i =0; i < laserSize; i++){
 			if (laserRange>msg->ranges[i])
 				laserRange = msg ->ranges[i];
+				laserIndex = i;
 		}
+		
 	}
 	if (laserRange == 11){
 		laserRange = 0;
 	}
-	
 
+	if (laserIndex - laserSize/2 - laserOffset != laserIndex-laserSize/2 + laserOffset){
+		if (laserIndex - laserSize/2 - laserOffset > laserIndex-laserSize/2 + laserOffset)
+			leftTurn = !leftTurn;
+		else
+			rightTurn = !rightTurn;	
+	}
+	
 }
 
 
@@ -94,7 +109,7 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 	posY = msg->pose.pose.position.y; 
 	yaw = tf::getYaw(msg->pose.pose.orientation);  // convert queration to euler angles
 
-	ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180/pi); // print pos and orientation for testing
+	//ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180/pi); // print pos and orientation for testing
 }
 
 
@@ -108,6 +123,8 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 
 int main(int argc, char **argv)
 {
+	std::cout<<"enter main cout"<<std::endl;
+	ROS_INFO ("entered main");
 	ros::init(argc, argv, "image_listener");   //init a node called image_listener
 	ros::NodeHandle nh; //
 	teleController eStop;
@@ -121,14 +138,14 @@ int main(int argc, char **argv)
 
 	//create a subscriber named bumper_sub; subscribe to topic named bumper; size of msg quene; ROS will call  bumperCallback() function whenever a new message arrives 
 	// note: odom topic defined in turtlebot
-	ros::Subscriber odom = nh.subscribe("/odom", 1, odomCallback); 
+	ros::Subscriber odom = nh.subscribe("odom", 1, odomCallback); 
 
 
  	//create a publisher named vel_pub; msg type is Twist; will send msg thru topic teleop;  size of the message queue is 1
 	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1); 
 
-	double angular;
-	double linear;
+	double angular = 0;
+	double linear = 0;
 
 	geometry_msgs::Twist vel; //define a msg, type is Twist, name is vel
 
@@ -138,20 +155,21 @@ int main(int argc, char **argv)
 
 
 
+	while(ros::ok() && secondsElapsed <=480 ){
+		ROS_INFO("Position: (%f, %f) Orientation: %f laseRange: %f .", posX, posY, yaw*180/pi, laserRange); // print pos and orientation for testing
 
-	while(ros::ok() && secondsElapsed <=489){
 		ros::spinOnce();  // invoke all callback funcs and publish msgs
 		//.....**E-STOP DO NOT TOUCH**.......
 		eStop.block();
 		//...................................
 
 		//fill with your code
-		if (posX < 0.5 && yaw < pi/12 && !bumperRight && !bumperLeft)
+	/*	if (posX < 0.5 && yaw < pi/12 &&!bumperRight && !bumperLeft)
 		{
 			angular = 0.0;
 			linear = 0.2;
 		}
-		else if (posX > 0.5 && yaw < pi/2 && !bumperRight && !bumperLeft && !bumperCenter)
+		else if (posX > 0.5 && yaw < pi/2 &&!bumperRight && !bumperLeft && !bumperCenter)
 		{
 			angular = pi/6;
 			linear = 0.0;
@@ -161,13 +179,34 @@ int main(int argc, char **argv)
 			angular = 0.0;
 			linear =0.0;
 		}
-		
+	*/
+
+	if(laserRange > 0.7){
+		angular= -0.07;
+		linear = 0.1;
+	}
+	if(laserRange <= 7 && rightTurn == 1){
+		angular=pi/2;
+		linear=0.0;
+	}
+
+	if(laserRange <= 7 && leftTurn == 1){
+		angular=-pi/2;
+		linear = 0.0;
+	}
+	
+
+	
+
+	
+
 
   		vel.angular.z = angular; 
   		vel.linear.x = linear;
 
   		vel_pub.publish(vel);   // vel_pub node publishes vel msg
-
+		leftTurn = 0;
+		rightTurn = 0;
 
 		secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
 	}
