e97d7928ad7b82492f6044c2ae2374d40c98e421
tianliz <46984855+tianliz@users.noreply.github.com>
2019-02-06 17:36:20 -0500

    Add files via upload

--- src/contest1_DK_Feb6.cpp
+++ src/contest1_DK_Feb6.cpp
@@ -29,9 +29,8 @@ bool bumperLeft = 0, bumperCenter = 0, bumperRight = 0;
 
 //LaserScan variables
 double laserRange = 10;
-int laserSize = 0, laserOffset = 0, desiredAngle = 30;
+int laserSize = 0, laserOffset = 0, desiredAngle = 20;
 int laserIndex;
-double laserFront = 11;
 
 bool leftTurn, rightTurn;
 int dist_to_left, dist_to_right;
@@ -46,6 +45,95 @@ int bigturn_counter = 0;
 // callback functions defined below
 // each func receive a msg param with a specific variable type
 // use global variable to share info back to main func (i.e. int main())
+
+
+
+void left_or_right(double laserRange, double left_most_dist, double right_most_dist, double mid_left_dist, double mid_right_dist, bool step1) {
+// decide left or right turn
+	// step 1
+	if (step1 == 1){
+	
+		if (laserRange <= 1){
+			if (mid_left_dist >= mid_right_dist && (mid_left_dist - mid_right_dist >0.1)) {
+				leftTurn = 1;
+			}
+			if (mid_left_dist < mid_right_dist && (mid_left_dist - mid_right_dist >0.1)) {
+				rightTurn = 1;
+			}
+		}
+	}
+
+	// step 2
+	else {
+		// TBD !!!!
+
+		if (laserRange <= 1){	
+			if (left_most_dist >= right_most_dist && (left_most_dist - right_most_dist >0.1)) {
+				leftTurn = 1;
+			}
+			if (left_most_dist < right_most_dist && (right_most_dist - left_most_dist >0.1)) {
+				rightTurn = 1;
+			}
+
+
+		
+		}
+
+	}
+
+
+}
+
+void turn_or_straight (double laserRange, bool step1){
+
+//ROS_INFO("inside turn_or_straight !!\n");
+
+	// if step 1, go around 
+	if (step1 == 1){
+		//ROS_INFO("inside step1 turn or straight !!\n");
+		if (laserRange <=0.7) {
+		//ROS_INFO("TOO CLOSE !!\n");
+
+		angular = pi/3;
+		linear = 0.0;
+		bigturn_counter++; 
+
+		}
+
+		if(laserRange > 1){
+		//	ROS_INFO("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS \n");
+			angular = 0;
+			linear = 0.1;
+		}
+
+		if(rightTurn == 1 && laserRange > 0.7){
+		//	ROS_INFO("RRRRRRRRRRRRRRRRRRRRRRRRRRRR \n");
+			angular= -pi/6;
+			linear=0.0;
+		}
+
+
+		if(leftTurn == 1 && laserRange > 0.7){
+			angular= pi/6;
+		//	ROS_INFO("LLLLLLLLLLLLLLLLLLLLLLLLLLLLL \n");
+
+			linear = 0.0;
+		}
+
+	//	ROS_INFO("INside: angular is : %f, linear is : %f", angular, linear);
+	}
+
+
+	// if step 2, decide what to response when see leftTurn = 1 or RightTurn = 1
+	if (step1 == 0){
+		
+		// TBD !!!!!
+	}
+
+}
+
+
+
 
 
 void bumperCallback(const kobuki_msgs::BumperEvent msg){
@@ -68,49 +156,77 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	//fill with your code
 	laserSize = (msg->angle_max - msg->angle_min)/msg->angle_increment;
 	laserOffset = desiredAngle*pi/(180*msg->angle_increment);
-	ROS_INFO("Size of laser scan array: %i and size of offset: %i", laserSize, laserOffset);// Print the size of the array and the offset for testing purposes.
-
+	ROS_INFO("Size of laser scan array: %i and size of offset: %i \n", laserSize, laserOffset);// Print the size of the array and the offset for testing purposes.
+	laserRange = 11;
 	// Below: distance measurement by searching the “ranges” array
-	// done by finding the smallest distance in the desired field of view
+	// done by finding the smallest distance in the desired field of view, in meters
 	// The desired field of view is defined by a negative and positive laseroffset from the heading angle of the robot.
 	//  If the desired field of view > sensor's, use sensor's instead
+// array index 0~639  0 320 639
+	
+/*
+	left_most_dist = msg->ranges[laserSize/2 - laserOffset+2]; // index only works for desiredAngle = 10  **********
+	right_most_dist = msg->ranges[laserSize/2 + laserOffset-2];
 
-	laserRange = 11;
-	laserFront = 11; 
+*/
 
-	if (desiredAngle*pi/180 <msg->angle_max && -desiredAngle*pi/180 >msg->angle_min){
-		for (int i =laserSize/2 - laserOffset; i<laserSize/2 + laserOffset; i++){
-			if (laserRange > msg->ranges[i])
-				laserRange = msg->ranges[i];
-				laserIndex = i;
+
+
+
+
+
+// Jan 31 added: 
+//ranges index fron turtlebot view: [600][599][][][499]][][][][1][0]
+
+										//^         ^
+										//|			|
+										//step2		step1
+										//left_most	 mid_left
+
+/* trial 1: with average
+	left_most_dist = (msg->ranges[laserSize/2 + laserOffset-2]+msg->ranges[laserSize/2 + laserOffset-3]+msg->ranges[laserSize/2 + laserOffset-4])/3;
+	right_most_dist = (msg->ranges[laserSize/2 - laserOffset+2]+msg->ranges[laserSize/2 - laserOffset+3]+msg->ranges[laserSize/2 - laserOffset+4])/3;
+
+*/
+
+// trial 2: without avgerage
+
+	left_most_dist = msg->ranges[laserSize/2 + laserOffset-2]; // step 2
+	right_most_dist = msg->ranges[laserSize/2 - laserOffset+2]; // step 2
+
+	mid_left_dist = msg->ranges[laserSize/2 + laserOffset-2 - 200]; // step 1, needs tuning
+ 	mid_right_dist = msg->ranges[laserSize/2 - laserOffset+2 + 200]; // step 1, needs tuning
+	
+
+// if step 2, use left_most and right_most to set laserRange
+	if (step1 == 0){
+		if (left_most_dist >= right_most_dist){
+			laserRange = right_most_dist;
 		}
+		else 
+			laserRange = left_most_dist;
+}
+
+	// if step 1, use mid_left and mid_right to set laserRange
+	if (step1 == 1){
+
+		if (mid_left_dist >= mid_right_dist){
+			laserRange = mid_right_dist;
+		}
+		else 
+			laserRange = mid_left_dist;
 	}
 
-	else
-	{
-		for ( int i =0; i < laserSize; i++){
-			if (laserRange>msg->ranges[i])
-				laserRange = msg ->ranges[i];
-				laserIndex = i;
-		}
-	}
-
-	if (laserRange == 11){
+	if (laserRange == 11){ 
 		laserRange = 0;
 	}
 
-	if (laserIndex - laserSize/2 - laserOffset != laserIndex-laserSize/2 + laserOffset){
-		if (laserIndex - laserSize/2 - laserOffset > laserIndex-laserSize/2 + laserOffset)
-			leftTurn = !leftTurn;
-		else
-			rightTurn = !rightTurn;	
-	}
-
-	if (laserIndex <= 374 && laserIndex >= 264 && laserRange != 0) {
-		laserFront = laserRange;
-	}
-		
 }
+
+
+
+
+
 
 
 //Odom Tutorial: http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
@@ -121,90 +237,6 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 	yaw = tf::getYaw(msg->pose.pose.orientation);  // convert queration to euler angles
 
 	//ROS_INFO("Position: (%f, %f) Orientation: %f rad or %f degrees.", posX, posY, yaw, yaw*180/pi); // print pos and orientation for testing
-}
-
-void left_or_right() {
-// decide left or right turn
-
-	if (laserRange <= 1){
-		if (laserIndex <= 264) {
-			leftTurn = 1;
-		}
-		if (laserIndex >= 374) {
-			rightTurn = 1;
-		}
-	}
-}
-
-int sgn(double v) {
-	if (v < 0) return -1;
-	if (v > 0) return 1;
-	return 0;
-}
-
-void turn(double angle) {
-
-	ros::NodeHandle nh;
-	ros::Subscriber odom = nh.subscribe("odom", 1, odomCallback); 
-	
- 	//create a publisher named vel_pub; msg type is Twist; will send msg thru topic teleop;  size of the message queue is 1
-	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1); 
-
-	geometry_msgs::Twist vel;
-
-	ros::spinOnce();
-	double yawGoal = yaw + angle;
-	//double yawOld = yaw;
-	if (yawGoal > pi) {
-		yawGoal = yawGoal - pi*2;
-	}
-	if (yawGoal < -pi) {
-		yawGoal = yawGoal + pi*2;
-	}
-	
-	while (yaw <= yawGoal) {
-
-		double angular = sgn(angle)*pi/6;
-		double linear = 0;
-
-		vel.angular.z = angular; 
-		vel.linear.x = linear;
-		vel_pub.publish(vel);
-		
-		ros::spinOnce();
-
-	}
-}
-
-void straight () {
-	angular = 0;
-	linear = 0.1;
-}
-
-void smallTurn(int turnDirection){
-	angular= sgn(turnDirection)*pi/6;
-	linear=0.0;
-}
-
-void turn_or_straight (int turnDirection){
-	// determine if the turtlebot need to do small turn, big turn or straight 
-	if(laserRange > 1){
-		//	ROS_INFO("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS \n");
-		straight();
-	}
-
-	if (laserFront <= 1 && laserFront != 0) { // turn left/right if face a wall
-		turn(sgn(turnDirection)*0.5*pi);
-		bigturn_counter++;
-	}
-
-	else if (rightTurn == 1 && laserFront > 1) { // slight right if too close to wall on the left
-		smallTurn(-1);
-	}
-
-	else if (leftTurn == 1 && laserFront > 1) { // slight right if too close to wall on the left
-		smallTurn(1);
-	}
 }
 
 
@@ -253,14 +285,22 @@ int main(int argc, char **argv)
 		//.....**E-STOP DO NOT TOUCH**.......
 		eStop.block();
 
-		left_or_right(); // determine if there is anything on the side;
-		
-		if (bigturn_counter <= 6){  // step 1
-			turn_or_straight (-1); // right turn if face a wall
+
+		// decide when to enter step2
+		if (bigturn_counter >2000){  // needs tuning
+			step1 = 0;
 		}
 
+		// set Leftturn ad rightturn to 1 
+		left_or_right(laserRange, left_most_dist, right_most_dist,mid_left_dist, mid_right_dist, step1);
 
-		//ROS_INFO("mid_left_dist: %f, mid_right_dist %f \n", mid_left_dist, mid_right_dist);
+		// response to different Leftturn and rightturn 
+		turn_or_straight (laserRange, step1);
+
+	// note: -ve angular speed -> cw
+
+
+		ROS_INFO("mid_left_dist: %f, mid_right_dist %f \n", mid_left_dist, mid_right_dist);
 		//ROS_INFO("angular is : %f, linear is : %f", angular, linear);
   		vel.angular.z = angular; 
   		vel.linear.x = linear;
