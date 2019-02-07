bacc57f21ff52d8b5b073611fa46082b7c8342c5
eltonzhang95 <44455408+eltonzhang95@users.noreply.github.com>
2019-02-06 22:27:56 -0500

    Not functioning version

--- src/contest1.cpp
+++ src/contest1.cpp
@@ -47,6 +47,10 @@ int bigturn_counter = 0;
 // each func receive a msg param with a specific variable type
 // use global variable to share info back to main func (i.e. int main())
 
+//step 2, 3
+double leftMost;
+double rightMost;
+double angleMax;
 
 void bumperCallback(const kobuki_msgs::BumperEvent msg){
 
@@ -58,7 +62,6 @@ else if (msg.bumper == 2)
 	bumperRight = !bumperRight;
 }
 
-
 // LaserScan msg: http://docs.ros.org/melodic/api/sensor_msgs/html/msg/LaserScan.html
 // Inside the laserCallback function calculate the size of the msg->ranges array. Also calculate the desired field of
 // view. This parameter will be used to reduce the number of indices in the array that we will consider when
@@ -68,7 +71,7 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	//fill with your code
 	laserSize = (msg->angle_max - msg->angle_min)/msg->angle_increment;
 	laserOffset = desiredAngle*pi/(180*msg->angle_increment);
-	ROS_INFO("Size of laser scan array: %i and size of offset: %i", laserSize, laserOffset);// Print the size of the array and the offset for testing purposes.
+	//ROS_INFO("Size of laser scan array: %i and size of offset: %i", laserSize, laserOffset);// Print the size of the array and the offset for testing purposes.
 
 	// Below: distance measurement by searching the “ranges” array
 	// done by finding the smallest distance in the desired field of view
@@ -80,18 +83,20 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 
 	if (desiredAngle*pi/180 <msg->angle_max && -desiredAngle*pi/180 >msg->angle_min){
 		for (int i =laserSize/2 - laserOffset; i<laserSize/2 + laserOffset; i++){
-			if (laserRange > msg->ranges[i])
+			if (laserRange > msg->ranges[i]){
 				laserRange = msg->ranges[i];
 				laserIndex = i;
+			}
 		}
 	}
 
 	else
 	{
 		for ( int i =0; i < laserSize; i++){
-			if (laserRange>msg->ranges[i])
+			if (laserRange>msg->ranges[i]){
 				laserRange = msg ->ranges[i];
 				laserIndex = i;
+			}
 		}
 	}
 
@@ -109,9 +114,13 @@ void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	if (laserIndex <= 374 && laserIndex >= 264 && laserRange != 0) {
 		laserFront = laserRange;
 	}
+
+	//step 2, step 3
+	leftMost = msg->ranges[638];
+	rightMost = msg->ranges[0];
+	angleMax=msg->angle_max;
 		
 }
-
 
 //Odom Tutorial: http://wiki.ros.org/navigation/Tutorials/RobotSetup/Odom
 
@@ -125,7 +134,6 @@ void odomCallback(const nav_msgs::Odometry::ConstPtr& msg){
 
 void left_or_right() {
 // decide left or right turn
-
 	if (laserRange <= 1){
 		if (laserIndex <= 264) {
 			leftTurn = 1;
@@ -172,7 +180,6 @@ void turn(double angle) {
 		vel_pub.publish(vel);
 		
 		ros::spinOnce();
-
 	}
 }
 
@@ -189,7 +196,6 @@ void smallTurn(int turnDirection){
 void turn_or_straight (int turnDirection){
 	// determine if the turtlebot need to do small turn, big turn or straight 
 	if(laserRange > 1){
-		//	ROS_INFO("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS \n");
 		straight();
 	}
 
@@ -207,19 +213,114 @@ void turn_or_straight (int turnDirection){
 	}
 }
 
+void step2(){ //turn right at intersections
 
+	ros::NodeHandle nh;
+	ros::Subscriber odom = nh.subscribe("odom", 1, odomCallback); 
+	
+ 	//create a publisher named vel_pub; msg type is Twist; will send msg thru topic teleop;  size of the message queue is 1
+	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1); 
+
+	geometry_msgs::Twist vel;
+
+	ros::spinOnce();
+	
+	double oldRight = rightMost; //initialize oldRight
+	double newRight;
+
+	for(;bigturn_counter<=12;) {
+		cout<<"step 2     ";
+		cout<<bigturn_counter;
+		cout<<"\n";
+
+		newRight= rightMost;
+
+		if(newRight-oldRight<0.5){ //no intersection
+			turn_or_straight(-1);
+
+			vel.angular.z = angular; 
+			vel.linear.x = linear;
+			vel_pub.publish(vel);
+			
+		}
+		else if(newRight-oldRight>=0.5){ //intersection
+			double currentX=posX;
+			while(abs(currentX-posX)<(oldRight+0.2)*cos(angleMax)){
+				straight(); 
+				
+				vel.angular.z = angular; 
+				vel.linear.x = linear;
+				vel_pub.publish(vel);
+				
+				ros::spinOnce();
+			}
+			turn(sgn(-1)*0.5*pi);
+			bigturn_counter++;
+		}
+
+		oldRight=newRight;
+		ros::spinOnce();
+	}
+}
+
+void step3(){ //turn left at intersections
+
+	ros::NodeHandle nh;
+	ros::Subscriber odom = nh.subscribe("odom", 1, odomCallback); 
+	
+ 	//create a publisher named vel_pub; msg type is Twist; will send msg thru topic teleop;  size of the message queue is 1
+	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1); 
+
+	geometry_msgs::Twist vel;
+
+	ros::spinOnce();
+
+	double oldLeft = leftMost;
+	double newLeft;
+
+	for(;bigturn_counter<=18;) {
+		cout<<"step 3     ";
+		cout<<bigturn_counter;
+		cout<<"\n";
+
+		newLeft=leftMost;
+
+		if(newLeft-oldLeft<0.5){ //no intersection
+			turn_or_straight(-1);
+
+			vel.angular.z = angular; 
+			vel.linear.x = linear;
+			vel_pub.publish(vel);
+		}
+		else if(newLeft-oldLeft>=0.5){ //intersection
+			double currentX=posX;
+			while(abs(currentX-posX)<(oldLeft+0.2)*cos(angleMax)){
+				straight(); 
+
+				vel.angular.z = angular; 
+				vel.linear.x = linear;
+				vel_pub.publish(vel);
+				
+				ros::spinOnce(); 
+			}
+			turn(sgn(1)*0.5*pi);
+			bigturn_counter++;
+		}
+
+		oldLeft=newLeft;
+		ros::spinOnce();
+	}
+}
 
 int main(int argc, char **argv)
 {
-	std::cout<<"enter main cout"<<std::endl;
-	ROS_INFO ("entered main");
 	ros::init(argc, argv, "image_listener");   //init a node called image_listener
 	ros::NodeHandle nh; //
 	teleController eStop;
 
 
 	//create a subscriber named map_sub, subscrite to topic named map within gmapping, 
-//	ros::Subscriber map_sub = nh.subscribe("map", 10, &mapCallback);
+    //ros::Subscriber map_sub = nh.subscribe("map", 10, &mapCallback);
 
 
 	//create a subscriber named bumper_sub; subscribe to topic named bumper; size of msg quene; ROS will call  bumperCallback() function whenever a new message arrives 
@@ -235,16 +336,11 @@ int main(int argc, char **argv)
  	//create a publisher named vel_pub; msg type is Twist; will send msg thru topic teleop;  size of the message queue is 1
 	ros::Publisher vel_pub = nh.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1); 
 
-//	double angular = 0;
-//	double linear = 0;
-
 	geometry_msgs::Twist vel; //define a msg, type is Twist, name is vel
 
 	std::chrono::time_point<std::chrono::system_clock> start;
 	start  = std::chrono::system_clock::now(); //start timer
 	uint64_t secondsElapsed = 0; //the timer just started, we know less than 480, no neded to change
-
-
 
 	while(ros::ok() && secondsElapsed <=480 ){
 	//	ROS_INFO("Position: (%f, %f) Orientation: %f laseRange: %f .", posX, posY, yaw*180/pi, laserRange); // print pos and orientation for testing
@@ -255,21 +351,28 @@ int main(int argc, char **argv)
 
 		left_or_right(); // determine if there is anything on the side;
 		
-		if (bigturn_counter <= 6){  // step 1
+		if (bigturn_counter <= -1){  // step 1
 			turn_or_straight (-1); // right turn if face a wall
 		}
+		else if (bigturn_counter>=0&&bigturn_counter<=12){
+			step2();
+		}
+		else if (bigturn_counter>12&&bigturn_counter<=18){
+			cout<<"step 3     ";
+			cout<<bigturn_counter;
+			cout<<"\n";
+			step3();
+		}
 
-
-		//ROS_INFO("mid_left_dist: %f, mid_right_dist %f \n", mid_left_dist, mid_right_dist);
-		//ROS_INFO("angular is : %f, linear is : %f", angular, linear);
   		vel.angular.z = angular; 
   		vel.linear.x = linear;
 
   		vel_pub.publish(vel);   // vel_pub node publishes vel msg
-		leftTurn = 0;
-		rightTurn = 0;
 
 		secondsElapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
+
+		leftTurn = 0;
+		rightTurn = 0;
 	}
 
 	return 0;
