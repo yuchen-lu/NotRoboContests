079cb378d32adc9d12060d599c9b5f8ea60899f5
tianliz <46984855+tianliz@users.noreply.github.com>
2019-03-07 11:22:32 -0500

    Add files via upload

--- contest2/src/contest2.cpp
+++ contest2/src/contest2.cpp
@@ -3,6 +3,27 @@
 #include <robot_pose.h>
 #include <imagePipeline.h>
 
+float BoxCoord[5][3];
+
+int FindTarget(float x, float y) {
+    
+    float d[5];
+
+    for(int i = 0; i < 5; i++) {
+        d[i] = sqrt((x - BoxCoord[i][0]) * (x - BoxCoord[i][0])  + (y - BoxCoord[i][1]) * (y - BoxCoord[i][1]));
+    }
+
+    int min = 0;
+
+    for(int k = 1; k < 5; k++) {
+        if (d[k] < d[min]){
+            min = k;
+        }
+    }
+
+    return min;
+}
+
 int main(int argc, char** argv) {
     // Setup ROS.
     ros::init(argc, argv, "contest2");
@@ -22,14 +43,50 @@ int main(int argc, char** argv) {
                   << boxes.coords[i][2] << std::endl;
     }
     // Initialize image objectand subscriber.
-    ImagePipeline imagePipeline(n);
+    // ImagePipeline imagePipeline(n);
     // Execute strategy.
+
+    for(int j = 0; j < boxes.coords.size(); ++j) {
+        BoxCoord[j][0] = boxes.coords[j][0];
+        BoxCoord[j][1] = boxes.coords[j][1];
+        BoxCoord[j][2] = boxes.coords[j][2];
+    }
+ 
+    Navigation navigation;
+
     while(ros::ok()) {
         ros::spinOnce();
-        /***YOUR CODE HERE***/
-        // Use: boxes.coords
-        // Use: robotPose.x, robotPose.y, robotPose.phi
-        imagePipeline.getTemplateID(boxes);
+
+        do {
+            float posX = robotPose.x;
+            float posY = robotPose.y;
+
+            std::cout << posX;
+            std::cout << "\n";
+            std::cout << posY;
+            std::cout << "\n";
+            std::cout << "\n";
+
+            int target = FindTarget(posX, posY);
+
+            std::cout << target;
+            std::cout << "\n";
+            std::cout << "\n";
+
+            double goalX = BoxCoord[target][0]+0.6*cos(BoxCoord[target][2]);
+            double goalY = BoxCoord[target][1]+0.6*sin(BoxCoord[target][2]);
+
+            std::cout << goalX;
+            std::cout << "\n";
+            std::cout << goalY;
+            std::cout << "\n";
+            std::cout << "\n";
+
+            Navigation::moveToGoal(goalX, goalY,  BoxCoord[target][2]);
+        }
+        while (false);
+
+        //imagePipeline.getTemplateID(boxes);
         ros::Duration(0.01).sleep();
     }
     return 0;
